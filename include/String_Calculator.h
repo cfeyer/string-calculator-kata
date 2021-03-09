91cdcac2aa0c740a80bbd1af8b354321cadfd21c
user <user@build01>
2021-03-08 21:59:52 -0500

    [Step 1]: Green: add("0") == 0.

--- include/String_Calculator.h
+++ include/String_Calculator.h
@@ -7,7 +7,7 @@ class String_Calculator
 {
 	public:
 
-		int add( const std::string & str ) {return str.empty() ? 0 : 1;}
+		int add( const std::string & str ) { return str.empty() ? 0 : std::stoi(str); }
 };
 
 #endif /*STRING_CALCULATOR_H*/
