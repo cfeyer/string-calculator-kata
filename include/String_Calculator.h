1221d5af9dd059767bba0ece20af5b1f1bd52869
user <user@build01>
2021-03-09 18:51:51 -0500

    [Step 1]: Green: add("1,2") == 3.

--- include/String_Calculator.h
+++ include/String_Calculator.h
@@ -3,11 +3,39 @@
 
 #include <string>
 
+#include <sstream>
+
 class String_Calculator
 {
 	public:
 
-		int add( const std::string & str ) { return str.empty() ? 0 : std::stoi(str); }
+		int add( const std::string & expression )
+		{
+			int accumulator = 0;
+			std::ostringstream token_buffer;
+
+			for( std::string::size_type i = 0; i < expression.size(); ++i )
+			{
+				char c = expression.at(i);
+
+				if( c != ',' )
+				{
+					token_buffer << c;
+				}
+				else
+				{
+					accumulator += std::stoi( token_buffer.str() );
+					token_buffer.str( "" );
+				}
+			}
+
+			if( !token_buffer.str().empty() )
+			{
+				accumulator += std::stoi( token_buffer.str() );
+			}
+
+			return accumulator;
+		}
 };
 
 #endif /*STRING_CALCULATOR_H*/
