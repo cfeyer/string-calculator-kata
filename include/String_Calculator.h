1652aee87b65c01e1de37588feb9a15d9ad5e53d
user <user@build01>
2021-03-08 21:39:46 -0500

    [Step 1]: Green: String_Calculator::add() takes string arg.

--- include/String_Calculator.h
+++ include/String_Calculator.h
@@ -1,8 +1,13 @@
 #ifndef STRING_CALCULATOR_H
-#define  STRING_CALCULATOR_H
+#define STRING_CALCULATOR_H
+
+#include <string>
 
 class String_Calculator
 {
+	public:
+
+		void add( const std::string & str ) {}
 };
 
 #endif /*STRING_CALCULATOR_H*/
