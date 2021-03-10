bee5c9e029651f8791a52da69e7bc96ac8fc0f26
user <user@build01>
2021-03-09 20:48:35 -0500

    [Step 3]: Refactor: Extract function 'split()'.

--- include/String_Calculator.h
+++ include/String_Calculator.h
@@ -2,12 +2,15 @@
 #define STRING_CALCULATOR_H
 
 #include <string>
+#include <vector>
 
 class String_Calculator
 {
 	public:
 
 		int add( const std::string & expression );
+
+		std::vector<std::string> split( const std::string & expression ) const;
 };
 
 #endif /*STRING_CALCULATOR_H*/
--- src/String_Calculator.cpp
+++ src/String_Calculator.cpp
@@ -1,16 +1,32 @@
 #include "String_Calculator.h"
 
 #include <sstream>
+#include <set>
 
 
 int String_Calculator::add( const std::string & expression )
 {
+	const auto tokens = split( expression );
 	int accumulator = 0;
+
+	for( const auto & token : tokens )
+	{
+		accumulator += std::stoi( token );
+	}
+
+	return accumulator;
+}
+
+
+std::vector<std::string> String_Calculator::split( const std::string & expression ) const
+{
+	std::set<char> delimiters = {',', '\n'};
+	std::vector<std::string> tokens;
 	std::ostringstream token_buffer;
 
 	for( char c : expression )
 	{
-		bool is_delimiter = ((c == ',') || (c == '\n'));
+		bool is_delimiter = (delimiters.find(c) != delimiters.end());
 
 		if( !is_delimiter )
 		{
@@ -18,16 +34,16 @@ int String_Calculator::add( const std::string & expression )
 		}
 		else
 		{
-			accumulator += std::stoi( token_buffer.str() );
+			tokens.push_back( token_buffer.str() );
 			token_buffer.str( "" );
 		}
 	}
 
 	if( !token_buffer.str().empty() )
 	{
-		accumulator += std::stoi( token_buffer.str() );
+		tokens.push_back( token_buffer.str() );
 	}
 
-	return accumulator;
+	return tokens;
 }
 
