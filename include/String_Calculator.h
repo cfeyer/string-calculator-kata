90cdaca942188f2da3baa0163249a58f55699198
user <user@build01>
2021-03-10 20:57:26 -0500

    [Step 8]: Green: Observer called with add() result.

--- include/String_Calculator.h
+++ include/String_Calculator.h
@@ -33,7 +33,7 @@ class String_Calculator
 		std::vector<std::string> split( const std::string & expression, const std::set<char> & delimiters ) const;
 		std::vector<int> get_addends( const std::string & expression, const std::set<char> & delimiters ) const;
 		void throw_if_negative_addends( const std::vector<int> & addends ) const;
-		void notify_add_occurred( const std::string & expression ) const;
+		void notify_add_occurred( const std::string & expression, int result ) const;
 
 		static const int delimiter_declaration_size = 4;
 		static const int custom_delimiter_pos = 2;
